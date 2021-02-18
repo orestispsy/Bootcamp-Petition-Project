90c707f8e2350ddbbb875215e928ccc43f58b39f
Orestis Psycharis <opsychar@gmail.com>
2021-02-18 09:01:46 +0100

    bloody cookie

--- signatures.sql
+++ signatures.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS signatures;
 
 CREATE TABLE signatures (
     id SERIAL PRIMARY KEY,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL
- --   signature TEXT NOT NULL CHECK (signature != '')
+    firstname VARCHAR(255) NOT NULL CHECK (firstname != ''),
+    lastname VARCHAR(255) NOT NULL CHECK (lastname != ''),
+   signature TEXT NOT NULL CHECK (signature != '')
 );
