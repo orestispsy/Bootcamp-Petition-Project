3fcb21d345a6a4dcbb46d79a52866cca438e7180
Orestis Psycharis <opsychar@gmail.com>
2021-02-23 09:39:48 +0100

    version 4.5

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.redirect("/login")
+    res.redirect("/login");
 });
 
 app.get("/register", (req, res) => {
@@ -39,14 +39,14 @@ app.post("/register", (req, res) => {
         req.body.password
     ) {
         hash(req.body.password).then((password_hash) => {
-                 db.addRegistration(
+            db.addRegistration(
                 `${req.body.firstname}`,
                 `${req.body.lastname}`,
                 `${req.body.email}`,
                 `${password_hash}`
             )
                 .then(({ rows }) => {
-                    console.log("THE ROWS ARE", rows)
+                    console.log("THE ROWS ARE", rows);
                     req.session.user_id = rows[0].id;
                     console.log("ID IS:", req.session.user_id);
                     res.redirect("/profile");
@@ -73,7 +73,7 @@ app.post("/login", (req, res) => {
     if (req.body.email && req.body.password) {
         db.getPasswordHashed(req.body.email)
             .then(({ rows }) => {
-                console.log("here you go", rows)
+                console.log("here you go", rows);
                 if (rows.length === 0) {
                     res.render("login", {
                         layout: "main",
@@ -82,34 +82,30 @@ app.post("/login", (req, res) => {
                             "* We cannot find such an Email or Password, please try again *",
                     });
                 }
-                    compare(req.body.password, rows[0].password_hash)
-                        .then((match) => {
-                            if (match) {
-                                req.session.user_id = rows[0].id;
-                                console.log(
-                                    "COOKIE USER ID",
-                                    req.session.user_id
-                                );
-                                db.getLoginSignature(rows[0].id)
-                                    .then(({ rows }) => {
-                                        console.log("LOGIN ROWS!", rows);
-                                        if (rows.length === 0) {
-                                            res.redirect("/petition");
-                                        } else {
-                                            console.log(rows[0].id);
-                                            req.session.signatureId =
-                                                rows[0].id;
-                                            console.log(
-                                                "COOKIE SIGNATURE ID",
-                                                req.session.signatureId
-                                            );
-                                            res.redirect("/thanks");
-                                        }
-                                    })
-                                    .catch((err) => console.log(err));
-                            }
-                        })
-                        .catch((err) => console.log(err));
+                compare(req.body.password, rows[0].password_hash)
+                    .then((match) => {
+                        if (match) {
+                            req.session.user_id = rows[0].id;
+                            console.log("COOKIE USER ID", req.session.user_id);
+                            db.getLoginSignature(rows[0].id)
+                                .then(({ rows }) => {
+                                    console.log("LOGIN ROWS!", rows);
+                                    if (rows.length === 0) {
+                                        res.redirect("/petition");
+                                    } else {
+                                        console.log(rows[0].id);
+                                        req.session.signatureId = rows[0].id;
+                                        console.log(
+                                            "COOKIE SIGNATURE ID",
+                                            req.session.signatureId
+                                        );
+                                        res.redirect("/thanks");
+                                    }
+                                })
+                                .catch((err) => console.log(err));
+                        }
+                    })
+                    .catch((err) => console.log(err));
             })
             .catch((err) => console.log(err));
     } else {
@@ -123,25 +119,25 @@ app.post("/login", (req, res) => {
 
 app.get("/profile", (req, res) => {
     res.render("profile", {
-    layout: "main",
+        layout: "main",
     });
 });
 
 app.post("/profile", (req, res) => {
     console.log("AGE IS", req.body.age);
     if (req.body.age == "") {
-        req.body.age = null
+        req.body.age = null;
     }
-        db.addUserProfile(
-            req.body.age,
-            req.body.city,
-            req.body.homepage,
-            req.session.user_id
-        )
-            .then(({ rows }) => {
-                res.redirect("/petition");
-            })
-            .catch((err) => console.log(err));
+    db.addUserProfile(
+        req.body.age,
+        req.body.city,
+        req.body.homepage,
+        req.session.user_id
+    )
+        .then(({ rows }) => {
+            res.redirect("/petition");
+        })
+        .catch((err) => console.log(err));
 });
 
 app.get("/petition", (req, res) => {
@@ -159,8 +155,11 @@ app.post("/petition", (req, res) => {
         db.addSignatures(req.session.user_id, `${req.body.signature}`)
             .then(({ rows }) => {
                 req.session.signatureId = rows[0].id;
-                console.log("Petition SIGNATURE id IS", req.session.signatureId);
-                console.log("PETITION ROWS", rows)
+                console.log(
+                    "Petition SIGNATURE id IS",
+                    req.session.signatureId
+                );
+                console.log("PETITION ROWS", rows);
                 res.redirect("/thanks");
             })
             .catch((err) => console.log(err));
@@ -175,16 +174,15 @@ app.post("/petition", (req, res) => {
 });
 
 app.get("/thanks", (req, res) => {
-   
-        db.getSignature(req.session.signatureId)
-            .then(({ rows }) => {
-                console.log("thanks ROWS", rows);
-                res.render("thanks", {
-                    layout: "main",
-                    thisSignature: rows[0].signature,
-                });
-            })
-            .catch((err) => console.log(err));
+    db.getSignature(req.session.signatureId)
+        .then(({ rows }) => {
+            console.log("thanks ROWS", rows);
+            res.render("thanks", {
+                layout: "main",
+                thisSignature: rows[0].signature,
+            });
+        })
+        .catch((err) => console.log(err));
 });
 
 app.get("/signers", (req, res) => {
@@ -194,7 +192,6 @@ app.get("/signers", (req, res) => {
             res.render("signers", {
                 layout: "main",
                 rows,
-                
             });
         })
         .catch((err) => console.log(err));
@@ -215,7 +212,7 @@ app.get("/profile/edit", (req, res) => {
 app.post("/profile/edit", (req, res) => {
     db.getUserToEdit(req.session.user_id)
         .then(({ rows }) => {
-            if (req.body.password){
+            if (req.body.password) {
                 hash(req.body.password).then((password_hash) => {
                     db.updateUserLogin(
                         req.body.firstname,
@@ -223,13 +220,8 @@ app.post("/profile/edit", (req, res) => {
                         req.body.email,
                         password_hash,
                         req.session.user_id
-                    )
-                        .then(({ rows }) => {
-                            res.redirect("/thanks");
-                        })
-                        .catch((err) => console.log(err));
+                    ).catch((err) => console.log(err));
                 });
-
             } else {
                 console.log("UPDATE LOGIN rows:", rows);
                 db.updateUserLogin(
@@ -238,15 +230,23 @@ app.post("/profile/edit", (req, res) => {
                     req.body.email,
                     rows[0].password_hash,
                     req.session.user_id
-                ).then(({ rows }) => {
-                    res.redirect("/thanks");
-                });
-            } 
+                )
+                .catch((err) => console.log(err));
+            }
+            db.updateUserProfile(
+                req.body.age,
+                req.body.city,
+                req.body.homepage,
+                req.session.user_id
+            )
+            .catch((err) => console.log(err));
+        })
+        .then(() => {
+            console.log(req.session.user_id);
+            res.redirect("/thanks");
         })
         .catch((err) => console.log(err));
 });
-
-
 
 app.get("/signers/:city", (req, res) => {
     console.log("city :", req.params.city);
